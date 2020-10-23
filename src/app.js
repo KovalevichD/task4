de3a9eed013505f378570f7877feba5e08a0b01c
KovalevichD <kovalevich.Demi@gmail.com>
2020-10-23 23:17:37 +0300

    add task4

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
 const userRouter = require('./resources/users/user.router');
+const boardRouter = require('./resources/boards/board.router');
+const taskRouter = require('./resources/tasks/task.router');
+const { handleError } = require('./utils/error');
+const logger = require('./utils/logger');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -19,6 +24,20 @@ app.use('/', (req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  const { method, url, params, body } = req;
+  logger.log(
+    'info',
+    `${method} ${url} ${JSON.stringify(params)} ${JSON.stringify(body)}`
+  );
+  next();
+});
+
 app.use('/users', userRouter);
+app.use('/boards', boardRouter);
+app.use('/boards', taskRouter);
+app.use((err, req, res, next) => {
+  handleError(err, res);
+});
 
 module.exports = app;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,18 @@
-const usersRepo = require('./user.memory.repository');
+const usersRepo = require('./user.db.repository');
+const tasksRepo = require('./../tasks/task.db.repository');
 
-const getAll = () => usersRepo.getAll();
+const getAll = async () => await usersRepo.getAll();
+const getById = async id => await usersRepo.getById(id);
+const createUser = async user => await usersRepo.createUser(user);
+const updateUser = async user => await usersRepo.updateUser(user);
+const deleteUser = async id => {
+  const deletedUser = usersRepo.deleteUser(id);
+  const tasks = await tasksRepo.getAllByUserId(id);
+  tasks.forEach(async task => {
+    task.userId = null;
+    await tasksRepo.updateTask(task);
+  });
+  return deletedUser;
+};
 
-module.exports = { getAll };
+module.exports = { getAll, getById, createUser, updateUser, deleteUser };
